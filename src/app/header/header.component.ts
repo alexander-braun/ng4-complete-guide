17b5c3caa5557bc78e5373626bf826c68f44be6d
Alex <alexanderbraun@freenet.de>
2020-10-28 22:54:31 +0100

    add routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,14 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  @Output() navigationChange: EventEmitter<any> = new EventEmitter();
-
+export class HeaderComponent {
   dropdownVisible = false;
 
   toggleVisible(): void {
     this.dropdownVisible = !this.dropdownVisible;
-  }
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  onNavigate(navigationTarget: string): void {
-    this.navigationChange.emit(navigationTarget);
   }
 }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,7 +10,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.scss'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
   dropdownVisible = false;
 
@@ -16,9 +19,18 @@ export class RecipeDetailComponent implements OnInit {
     this.dropdownVisible = !this.dropdownVisible;
   }
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
 
   onAddToShoppingList(): void {
     this.shoppingListService.addIngredients(this.recipe.ingredients);
