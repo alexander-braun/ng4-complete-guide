f4a0f44fbe79e3147d3013f519bb4e9130289592
Alex <alexanderbraun@freenet.de>
2020-10-29 19:56:06 +0100

    replace emitter with subjects + cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  navigationTarget = 'recipe';
-
-  onNavigationChange(event: string): void {
-    this.navigationTarget = event;
-  }
-}
+export class AppComponent {}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,9 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel',
